38ee78ff4908c7beca024c3c2c96ed23894cf07a
ponttor <alex@heimstudio.ru>
2020-10-18 20:29:50 +0200

    references fixing

--- src/gendiff.js
+++ src/gendiff.js
@@ -1,3 +1,8 @@
-commander
-  .option('-s, --source <path>', 'source file')
-  .option('-l, --list [items]', 'value list', toArray, [])
+#!/usr/bin/env node
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .version('0.0.1')
+  .description('Compares two configuration files and shows a difference.')
