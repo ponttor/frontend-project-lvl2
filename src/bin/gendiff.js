7546ebe7b034d351002f8169e495d6bdcf8b049c
ponttor <alex@heimstudio.ru>
2020-10-20 18:01:54 +0200

    options and argumets added

--- src/bin/gendiff.js
+++ src/bin/gendiff.js
@@ -6,6 +6,8 @@ const program = new Command();
 
 program
 .version('0.0.1', '-v, --vers', 'output the version number')
-.description('Compares two configuration files and shows a difference.');
+.description('Compares two configuration files and shows a difference.')
+.option('-f, --format [type]', 'Output format')
+.arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);
