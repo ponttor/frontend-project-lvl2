f8a58775c37da8d3bb59316c68cca08b17cd1e35
ponttor <alex@heimstudio.ru>
2020-11-07 13:15:27 +0100

    tests covering

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,17 +1,17 @@
 // import { strict as assert } from 'assert';
 import { test, expect } from '@jest/globals';
-import path from 'path';
+// import path from 'path';
 import fs from 'fs';
 import getDiff from '../src/getDiff';
 
 // assert(getDiff('file_source/file1.json', 'file_source/file2.json') ===
 // fs.readFileSync('file_source/fileDiff.txt', 'utf-8'));
 
-const getPath = (name) => path.join(__dirname, name);
-const plain1 = getPath('file_source/file1.json');
-const plain2 = getPath('file_source/file2.json');
-console.log(plain1);
-console.log(plain2);
+// const getPath = (name) => path.join(__dirname, name);
+// const filePath1 = getPath('file_source/file1.json');
+// const filePath2 = getPath('file_source/file2.json');
+// console.log(filePath1);
+// console.log(filePath2);
 
 test('compare json', () => {
   expect(getDiff('file_source/file1.json', 'file_source/file2.json')).toEqual(fs.readFileSync('file_source/fileDiff.txt', 'utf-8'));
